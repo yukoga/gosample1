2dcb9c1f6e9de8e22eb64c7dbfc8c70ab37d1bfa
yukoga <yukoga@gmail.com>
2014-12-02 01:59:59 +0900

    add more sample code.

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/yukoga/newmath"
+)
+
+func swap(x, y string) (string, string) {
+  return y, x
+}
+
 
 func main() {
-  fmt.Printf("Hello, Go world.\n")
+  fmt.Printf("Hello, Go world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+  fmt.Printf("Hello, Go world. power_n(2,0) = %v\n", newmath.Power_n(2,0))
+  fmt.Printf("Hello, Go world. power_n(2,1) = %v\n", newmath.Power_n(2,1))
+  fmt.Printf("Hello, Go world. power_n(2,2) = %v\n", newmath.Power_n(2,2))
+  fmt.Printf("Hello, Go world. power_n(2,3) = %v\n", newmath.Power_n(2,3))
+  a, b:=swap("Hello", "world")
+  fmt.Printf("Hello, Go world. \n %s, %s. \n", a, b)
 }
